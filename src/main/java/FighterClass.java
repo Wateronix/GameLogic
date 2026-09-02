b72637cb2f6d305a196f2d98d39845dd3a9c46ca
sensenmennel <xeon-aureum@gmx.de>
2026-09-02 16:33:51 +0200

    better fighter classes

--- src/main/java/FighterClass.java
+++ src/main/java/FighterClass.java
@@ -1,8 +1,16 @@
 public enum FighterClass {
-    BARBARIAN("Barbarian", "RAAAAHHHH!", 50, 25),
-    FENCER("Fencer", "En Garde!", 50, 25),
-    KNIGHT("Knight", "Deus Vult!", 50, 25),
-    BLADEDANCER("Bladedancer", "...", 50, 25);
+    BARBARIAN("Barbarian",
+            "Born in the harsh north, they learned to fight before they learned to speak. Their attacks deal double damage.",
+            60, 20),
+    FENCER("Fencer",
+            "Trained as a noble, exiled when their house fell. Yet their blade is as sharp as ever and their technique remains flawless. One additional free action each turn.",
+            45, 28),
+    KNIGHT("Knight",
+            "In the honor of the Sun God, they took up arms and armor to fight back the darkness wherever it lies. Every second Block action is free.",
+            55, 22),
+    BLADEDANCER("Bladedancer",
+            "Raised by a dark cult to fight with unmatched speed and ferocity, they now wield their blades only for themselves. Every second Attack action is free.",
+            50, 25);
 
     private final String displayName;
     private final String description;
